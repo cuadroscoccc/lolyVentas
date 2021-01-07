07b61d5500c6ea307ff2007a8bd2d3c193fcc1ab
cuadroscoccc <cindy.cuadrosco@gmail.com>
2021-01-07 14:54:35 -0500

    version movil

--- app/index.js
+++ app/index.js
@@ -1,10 +1,14 @@
-/* import {App} from './App.js'; */
+
 import {hamburgerMenu} from './helpers/hamburgerMenu.js';
 document.addEventListener("DOMContentLoaded", e=>{
-
 hamburgerMenu(".header-ico-btn",".header-nav");
 
 })
 
+/*  const inmuebles = document.querySelector(".inmuebles");
+  inmuebles.addEventListener("click", e =>{
+  location.href= "inmuebles.html";
+}) */
 
 
+console.log(location);
